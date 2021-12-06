e06161d4443db4c9b3ae4baf04af967e56d4f3d1
XA <xa@mes3.in>
2021-12-07 00:44:17 +0530

    Test different path

--- docs/conf.py
+++ docs/conf.py
@@ -11,7 +11,7 @@ import sys
 import guzzle_sphinx_theme
 
 sys.path.insert(0, os.path.abspath("."))
-sys.path.insert(0, os.path.abspath("./xautic/"))
+sys.path.insert(1, os.path.abspath("./xautic"))
 
 from xautic import __version__
 
